d7953843c48b67312514dc3ee0adcb5f1370cd30
Rob Halff <rob.halff@gmail.com>
2014-04-07 23:55:53 +0200

    async

--- nodes/read/node.js
+++ nodes/read/node.js
@@ -1,6 +1,13 @@
-output = [
-  input.repo,
-  'read',
-  input.branch,
-  input.path
-]
+on.input.path = function() {
+  input.repo.read(
+    input.branch,
+    input.path,
+    function(err, content, sha) {
+      output({
+        error: err,
+        content: content,
+        sha: sha
+      });
+    }
+  );
+};
