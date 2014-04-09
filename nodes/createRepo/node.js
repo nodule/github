92849774df3b8fb3063cf7f2e43a0a4dff3196ac
Rob Halff <rob.halff@gmail.com>
2014-04-09 03:39:58 +0200

    update

--- nodes/createRepo/node.js
+++ nodes/createRepo/node.js
@@ -1,6 +1,6 @@
 on.input.github = function() {
 
-  var repo = new input.github.Repository(input.user, input.name);
+  var repo = data.getRepo(input.user, input.name);
   input.options.name = input.name;
   repo.createRepo(input.options, function(err, result) {
     if(err) {
