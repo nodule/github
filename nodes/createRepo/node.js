921d3fe97863208c4a8275aec34cf2175b817307
Rob Halff <rob.halff@gmail.com>
2014-04-09 03:27:55 +0200

    async

--- nodes/createRepo/node.js
+++ nodes/createRepo/node.js
@@ -1,6 +1,7 @@
 on.input.github = function() {
 
-  var repo = new input.github.getRepo();
+  var repo = new input.github.Repository(input.user, input.name);
+  input.options.name = input.name;
   repo.createRepo(input.options, function(err, result) {
     if(err) {
       output({ error: err });
